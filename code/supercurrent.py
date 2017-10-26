b02458278c45be4026e635c8b59fbf28e53f53da
NeroCorleone <nefta.kanilmaz@gmail.com>
2017-10-26 14:39:42 +0200

    added new results

--- code/supercurrent.py
+++ code/supercurrent.py
@@ -14,7 +14,7 @@ import os
 from functools import partial
 import csv
 
-vsg_values = [0.0, ]#[-0.15, -0.25, -0.35, -0.375, -0.4, -0.425, -0.45, -0.475]
+vsg_values = [-0.1, -0.2, -0.3, -0.4, -0.5, -0.6]
 vbg = 0.8 
 nb_points = 1000 
 maxB = 0.0001
