95db6e05dfedd99048d2dcff7364786ac4441e7c
NeroCorleone <nefta.kanilmaz@gmail.com>
2017-10-27 14:53:58 +0200

    added new results

--- code/supercurrent.py
+++ code/supercurrent.py
@@ -14,10 +14,10 @@ import os
 from functools import partial
 import csv
 
-vsg_values = [-0.1, -0.2, -0.3, -0.4, -0.5, -0.6]
+vsg_values = [-0.1, -0.2, -0.25, -0.3, -0.35,-0.4, -0.45, -0.5, -0.6]
 vbg = 0.8 
-nb_points = 400 
-maxB = 0.0001
+nb_points = 1000 
+maxB = 0.0003
 magnetic_field = np.linspace(-maxB, maxB, nb_points)
 maxPhi = np.pi
 phase = (-np.pi, np.pi) 
@@ -30,7 +30,7 @@ at = 5.0
 a = 0.4
 
 pot_decay = 0 #QPC 20
-case = 'wg3_double'
+case = 'wg3_2'
 mainpath = '/users/tkm/kanilmaz/thesis/'
 setups = {'hb': ('results/hb/supercurrent/', 'designfiles/halfBarrier.png'),
           'hb_lower': ('results/hb_lower/supercurrent/', 'designfiles/hb_lower_part.png'),
