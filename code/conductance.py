9f435963b266d8d2396c59ba16ece959fb843131
NeroCorleone <nefta.kanilmaz@gmail.com>
2017-11-08 14:07:11 +0100

    added new results

--- code/conductance.py
+++ code/conductance.py
@@ -15,8 +15,8 @@ from functools import partial
 import csv
 
 nb_points = 500 
-splitgate_voltage = np.linspace(-1, 0.0, nb_points)
-vbg_values = [-0.2, -0.6,-0.4, -0.5, -0.3]#[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]#np.round(np.linspace(0.4, 0.9, 5), 2) 
+splitgate_voltage = np.linspace(0.0, 1.0, nb_points)
+vbg_values = [-0.2, -0.3, -0.4, -0.5, ]#[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]#np.round(np.linspace(0.4, 0.9, 5), 2) 
 phase = (-np.pi, np.pi) 
 
 delta = 1.0 
