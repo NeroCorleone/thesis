3dd7603b75092f572cec866e53175a61489edcda
NeroCorleone <nefta.kanilmaz@gmail.com>
2018-01-25 19:38:41 +0100

    fixed bug in supercurrent-armchair.py

--- code/supercurrent-armchair.py
+++ code/supercurrent-armchair.py
@@ -14,11 +14,11 @@ import os
 from functools import partial
 import csv
 
-vsg_values = [-0.1, -0.2]#np.arange(-0.0, -0.1, -0.01)
+vsg_values = [-0.2,]#np.arange(-0.0, -0.1, -0.01)
 vbg = 0.2 
 vlead = 0.0
-nb_points = 500 
-max_b = 0.00009
+nb_points = 100 
+max_b = 0.00005
 magnetic_field = np.linspace(- max_b, max_b, nb_points)
 maxPhi = np.pi
 phase = (-np.pi, np.pi) 
@@ -237,7 +237,7 @@ def superCurrent(scatMatrix, phi):
 
 def findMax(func, phase_min, phase_max):
     current = [func(phi) for phi in np.linspace(phase_min, phase_max)]
-    currentPeak = max(current, key=lambda x: x[2])
+    currentPeak = max(current, key=lambda x: x[0])
     #currentPeak = np.amax(current)
     return(currentPeak)
 
